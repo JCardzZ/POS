9754382bba78767b228982c495dafacc70573765
JCardzZ <cardozamanuel96@gmail.com>
2019-08-21 23:26:59 -0600

    CAMBIO DE NOMBRE
    SE SOLICITO UN CAMBIO PARA LA APP

--- Man/src/man/Man.java
+++ Man/src/man/Man.java
@@ -3,7 +3,7 @@ package man;
 public class Man {
 
     public static void main(String[] args) {
-        System.out.println("Hola Mundo");
+        System.out.println("Hola Manuel de Jesus Cardoza Cardoza 1111");
     }
 
 }
